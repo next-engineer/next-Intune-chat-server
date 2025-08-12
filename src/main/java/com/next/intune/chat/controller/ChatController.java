2cf566771b44d2df05a3e3a3413528998ba0d70c
cw7430 <cw282@naver.com>
2025-08-12 22:58:20 +0900

    쿠키 기반 ìswagger header 기반 인증 가능하게 설정

--- src/main/java/com/next/intune/chat/controller/ChatController.java
+++ src/main/java/com/next/intune/chat/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.next.intune.chat.controller;
 
 import com.next.intune.chat.service.ChatService;
 import com.next.intune.common.api.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chat")
+@Tag(name = "Chat Controller", description = "채팅 API")
 public class ChatController {
 
     private final ChatService chatService;
 
     @GetMapping("/match")
+    @Operation(summary = "채팅 매칭", description = "이메일과 비밀번호을 입력해주세요.")
     public ResponseEntity<ApiResult<?>> match(HttpServletRequest request) {
         chatService.match(request);
         return ResponseEntity.ok(ApiResult.success());
