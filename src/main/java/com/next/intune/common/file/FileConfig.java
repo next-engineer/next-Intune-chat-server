b7d5233a265f8b1b097bf206e1e14500065ce31f
cw7430 <cw282@naver.com>
2025-08-14 00:25:33 +0900

    사진 디렉토리

---
+++ src/main/java/com/next/intune/common/file/FileConfig.java
@@ -0,0 +1,18 @@
+package com.next.intune.common.file;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class FileConfig implements WebMvcConfigurer {
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/chat/**")
+                .addResourceLocations("file:///" + uploadDir.replace("\\", "/") + "/");
+    }
+}
