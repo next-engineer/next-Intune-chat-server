6d2b0e96feee3ebc412d151c4eb69ebc2e78618e
cw7430 <cw282@naver.com>
2025-08-13 14:35:48 +0900

    매칭 불러오기

--- src/main/java/com/next/intune/chat/controller/ChatController.java
+++ src/main/java/com/next/intune/chat/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.next.intune.chat.controller;
 
+import com.next.intune.chat.dto.response.MatchResponseDto;
+import com.next.intune.chat.entity.Match;
 import com.next.intune.chat.service.ChatService;
 import com.next.intune.common.api.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,8 +11,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,10 +26,16 @@ public class ChatController {
 
     private final ChatService chatService;
 
-    @GetMapping("/match")
+    @PostMapping("/match")
     @Operation(summary = "채팅 매칭")
     public ResponseEntity<ApiResult<?>> match(HttpServletRequest request) {
         chatService.match(request);
         return ResponseEntity.ok(ApiResult.success());
+    }
+
+    @GetMapping("/match")
+    @Operation(summary = "채팅 채팅 불러오기")
+    public ResponseEntity<ApiResult<List<MatchResponseDto>>> getMatches(HttpServletRequest request) {
+        return ResponseEntity.ok(ApiResult.success(chatService.getMatches(request)));
     }
 }
